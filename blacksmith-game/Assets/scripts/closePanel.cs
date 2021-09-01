a1fad83926b57c22ca239b8da9a9ef562ec772e9
Aakaash Rohra <aakrohra@gmail.com>
2021-09-01 03:22:09 -0400

    pop-up menu when right click anvil, close button within menu
    this took way too long to figure out

---
+++ blacksmith-game/Assets/scripts/closePanel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class closePanel : MonoBehaviour
+{
+
+    public GameObject panel;
+
+    public void panelClose() {
+      if (panel.active != false) {
+        panel.SetActive(false);
+      }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ blacksmith-game/Assets/scripts/rightClickPopUp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rightClickPopUp : MonoBehaviour
+{
+
+    public GameObject panel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+    }
+
+    void OnMouseOver() {
+      if (Input.GetMouseButtonDown(1)) {
+          panel.SetActive(true);
+      }
+    }
+
+}

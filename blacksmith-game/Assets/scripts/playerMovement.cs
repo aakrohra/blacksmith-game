5606d8d5ae9d019a6326111cdb050d39029ba1ab
Aakaash Rohra <aakrohra@gmail.com>
2021-09-01 01:32:27 -0400

    basic movement + anvil art

---
+++ blacksmith-game/Assets/scripts/playerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+  Rigidbody2D body;
+  SpriteRenderer renderer;
+
+  float horizontal;
+  float vertical;
+  float moveLimiter = 0.7f;
+
+  public float runSpeed = 20.0f;
+
+  void Start () {
+    body = GetComponent<Rigidbody2D>();
+    renderer = GetComponent<SpriteRenderer>();
+  }
+
+  void Update() {
+    horizontal = Input.GetAxisRaw("Horizontal");
+    vertical = Input.GetAxisRaw("Vertical");
+
+    if (horizontal > 0) {
+      renderer.flipX = false;
+    }
+    if (horizontal < 0) {
+      renderer.flipX = true;
+    }
+
+  }
+
+  void FixedUpdate() {
+    if (horizontal != 0 && vertical != 0) {
+      horizontal *= moveLimiter;
+      vertical *= moveLimiter;
+    }
+
+    body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+
+  }
+}
